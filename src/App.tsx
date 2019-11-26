81838ebaa28847f3a2d8fe404e3f2f598fb46ba8
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-11-26 15:24:30 +0100

    Add input css, add function to complete todo

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,24 @@ import React, { useState } from 'react';
 import './App.css';
 
 interface TodoProps {
-  todo: {text: string},
+  todo: {
+    text: string,
+    isCompleted: boolean
+  },
+  index: number,
+  completeTodo: (index: number) => void,
 }
 
-const Todo: React.FC<TodoProps> = ({todo}) => <div className="todo">{todo.text}</div>
+const Todo: React.FC<TodoProps> = ({todo, index, completeTodo}) => {
+  return (
+    <div style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }} className="todo">
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
+    </div>
+  )
+}
 
 const TodoForm: React.FC<{addTodo: (text:string) => void}> = ({addTodo}) => {
   const [value, setValue] = useState("")
@@ -19,20 +33,26 @@ const TodoForm: React.FC<{addTodo: (text:string) => void}> = ({addTodo}) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)} />
+      <input type="text" className="input" placeholder="Add Todo..." value={value} onChange={e => setValue(e.target.value)} />
     </form>
   )
 }
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState([
-    { text: "Learn about React" },
-    { text: "Meet friend for lunch" },
-    { text: "Build really cool todo app" }
+    { text: "Learn about React", isCompleted: true },
+    { text: "Meet friend for lunch", isCompleted: true },
+    { text: "Build really cool todo app", isCompleted: false }
   ])
 
   const addTodo = (text: string) => {
-    const newTodos = [...todos, {text}]
+    const newTodos = [...todos, {text, isCompleted: false}]
+    setTodos(newTodos)
+  }
+
+  const completeTodo = (index: number) =>{
+    const newTodos = [...todos]
+    newTodos[index].isCompleted = true
     setTodos(newTodos)
   }
 
@@ -40,7 +60,7 @@ const App: React.FC = () => {
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} todo={todo} />
+          <Todo key={index} index={index} todo={todo}  completeTodo={completeTodo}/>
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
