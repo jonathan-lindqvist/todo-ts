00a1b95aea694ac3dc0aed1a37a86c0c62068355
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-11-26 15:58:26 +0100

    Updates

---
+++ precache-manifest.2cecbd82c146cf9c80d2cdcbea5aaef0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e23f8964dbf41318ac216f20172056c",
+    "url": "/todo-ts/index.html"
+  },
+  {
+    "revision": "f4925568089a8aff5da3",
+    "url": "/todo-ts/static/css/main.2ff19a5e.chunk.css"
+  },
+  {
+    "revision": "2eccce1df201a71203a8",
+    "url": "/todo-ts/static/js/2.7898e9c6.chunk.js"
+  },
+  {
+    "revision": "f4925568089a8aff5da3",
+    "url": "/todo-ts/static/js/main.e08cb8ed.chunk.js"
+  },
+  {
+    "revision": "2db4723c1bd90fe93643",
+    "url": "/todo-ts/static/js/runtime-main.0d3065e6.js"
+  }
+]);
